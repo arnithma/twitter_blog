0fe6fdec4da88bf47c6aad9ce831cf5f6237fe70
Zachary R. Hamlin <hamlinzach@gmail.com>
2019-10-28 20:39:20 -0500

    customised devise views added name username etc...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+	# Prevent CSRF attacks by raisning an exception.
+	# for APIs, you may want to use :null_session instead.
+	protect_from_forgery with: :exception
+
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	protected
+
+	def configure_permitted_parameters
+		devise_parameters_sanitizer.permit(:sign_up, keys: [:username, :name, :bio, :location])
+		devise_parameters_sanitizer.permit(:account_update, keys: [:username, :name, :bio, :location])
+	end
 end
+
