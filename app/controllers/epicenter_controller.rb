91c7b4e94718c0ab6ff3047e0266f28945169587
Zachary R. Hamlin <hamlinzach@gmail.com>
2019-11-06 19:02:26 -0600

    update repo

--- app/controllers/epicenter_controller.rb
+++ app/controllers/epicenter_controller.rb
@@ -2,7 +2,7 @@ class EpicenterController < ApplicationController
   def feed
   	@following_tweets= []
 
-  	Tweet.all.each do |tweet|
+  	Tweet.all.each do |tweet| 
   		if current_user.following.include?(tweet.user_id) || current_user.id == tweet.user_id
   			@following_tweets.push(tweets)
   		end
@@ -10,11 +10,18 @@ class EpicenterController < ApplicationController
   end
 
   def show_user
+  	@user =  User.find(params[:id])
   end
 
   def now_following
+  	current_user.following.push(params[:id].to_i)
+  	current_user.save
+  	redirect_to show_user_path(id: params[:id])
   end 
 
   def unfollow
+  	current_user.following.delete(params[:id].to_i)
+  	current_user.save
+  	redirect_to show_user_path(id: params[:id])
   end
 end
